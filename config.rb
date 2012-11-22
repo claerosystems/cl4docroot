bc4aaf0ebdbcfae8f8ba7656d44696944bd21d36
Darryl Hein <dhein@xmmedia.com>
2012-11-22 10:21:17 -0700

    a few SASS config tweaks

--- config.rb
+++ config.rb
@@ -1,3 +1,9 @@
+# Compass comfig created with, run from the root of the site. Some additional config tweaks were made after.
+# compass create html/cl4/ --prepare --sass-dir css/sass --css-dir css --javascripts-dir js --images-dir images --output-style compressed --no-line-comments
+# to compile, use the following when inside the cl4 dir:
+# compass compile . --time --environment development
+# see the xm template site for how to use this within a project
+
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
@@ -5,7 +11,10 @@ http_path = "/"
 css_dir = "css"
 sass_dir = "css/sass"
 images_dir = "images"
+http_images_path = "/cl4/images"
+http_generated_images_path = "/cl4/images"
 javascripts_dir = "js"
+http_javascripts_path = "/cl4/js"
 
 output_style = :compressed
 
