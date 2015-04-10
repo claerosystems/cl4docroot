9dc7229de585a1e1f7f7edd403a453881a9741dd
Craig Nakamoto <craig.nakamoto@claero.com>
2015-04-10 13:06:16 -0400

    trying to consolidate css and js further

---
+++ js/combined.js
@@ -0,0 +1,5 @@
+// @codekit-append "/vendor/jquery-2.1.1.min.js";
+// @codekit-append "/vendor/jquery.mobile-1.4.5.min.js";
+// @codekit-append "/vendor/datepicker.js";
+// @codekit-append "/vendor/jquery.mobile.datepicker.js";
+// @codekit-append "/base.js";
